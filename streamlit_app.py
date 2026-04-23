4f39e8e145e168c1afb0abd2baae669e8f5069de
Lindsey <133406662+lab3ee@users.noreply.github.com>
2026-04-23 07:24:47 +0000

    updated formatting

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,13 +28,20 @@ if st.checkbox('Show raw data'):
     st.subheader('Raw data')
     st.write(data)
 
-st.subheader('Number of pickups by hour')
-hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-st.bar_chart(hist_values)
+tab1, tab2 = st.tabs(['Histogram', 'Map'])
+with tab1:
+    st.subheader('Number of pickups by hour')
+    col1, col2 = st.columns(2, border=True)
+    with col1:
+        hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+        st.bar_chart(hist_values)
 
-# Some number in the range 0-23
-hour_to_filter = st.slider('hour', 0, 23, 17)
-filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
 
-st.subheader('Map of all pickups at %s:00' % hour_to_filter)
-st.map(filtered_data)
+    with col2:
+        # Some number in the range 0-23
+        hour_to_filter = st.slider('hour', 0, 23, 17)
+        filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+
+with tab2:
+    st.subheader('Map of all pickups at %s:00' % hour_to_filter)
+    st.map(filtered_data)
