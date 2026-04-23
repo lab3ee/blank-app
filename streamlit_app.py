3413623583281f494197305089b2beaea516e432
Lindsey <133406662+lab3ee@users.noreply.github.com>
2026-04-23 07:05:45 +0000

    added subheader

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 
 st.title('Uber pickups in NYC')
-
+st.header('A visualization of rides in New York City')
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
          'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
